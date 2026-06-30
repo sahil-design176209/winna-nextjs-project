3dde5373439a8cc3033d53fb2b52b9ac9699c952
sahil-design176209 <sahil01@yrjtechnology.com>
2026-06-30 12:38:06 +0530

    betslip design fixed

--- app/event/EventComponent.tsx
+++ app/event/EventComponent.tsx
@@ -84,10 +84,11 @@ export default function EventComponent() {
           <div className="flex items-center gap-6">
             <button
               onClick={() => setActiveTab("all")}
-              className={`rounded-full px-4 py-2 text-[14px] font-semibold ${activeTab === "all"
+              className={`rounded-full px-4 py-2 text-[14px] font-semibold ${
+                activeTab === "all"
                   ? "bg-body-level-7 text-white"
                   : "text-typography-dark-gray"
-                }`}
+              }`}
             >
               All{" "}
               <span className="ml-1 text-[12px] bg-body-level-4 rounded-full text-typography-gray px-1.5 py-0">
@@ -97,8 +98,9 @@ export default function EventComponent() {
 
             <button
               onClick={() => setActiveTab("market")}
-              className={`text-[14px] ${activeTab === "market" ? "text-white" : "text-typography-gray"
-                }`}
+              className={`text-[14px] ${
+                activeTab === "market" ? "text-white" : "text-typography-gray"
+              }`}
             >
               Market{" "}
               <span className="text-[12px] bg-gray-800 text-typography-gray rounded-full px-1.5 py-0">
@@ -108,8 +110,9 @@ export default function EventComponent() {
 
             <button
               onClick={() => setActiveTab("odds")}
-              className={`text-[14px] ${activeTab === "odds" ? "text-white" : "text-typography-gray"
-                }`}
+              className={`text-[14px] ${
+                activeTab === "odds" ? "text-white" : "text-typography-gray"
+              }`}
             >
               Odds{" "}
               <span className="text-[12px] bg-gray-800 text-typography-gray rounded-full px-1.5 py-0">
@@ -314,6 +317,7 @@ export default function EventComponent() {
                 ].map((item) => (
                   <div
                     key={item.team}
+                    onClick={() => setSelectedBet(item)}
                     className="flex items-center justify-between rounded-lg bg-body-level-9 px-4 py-3 h-[30px]"
                   >
                     <span className="text-typography-gray text-[12px] font-bold">
@@ -398,6 +402,7 @@ export default function EventComponent() {
                 ].map((item) => (
                   <div
                     key={item.team}
+                    onClick={() => setSelectedBet(item)}
                     className="flex items-center justify-between rounded-lg bg-body-level-9 px-4 py-3 h-[30px]"
                   >
                     <span className="text-typography-gray text-[12px] font-bold">
@@ -414,7 +419,7 @@ export default function EventComponent() {
         )}
       </section>
       <div className="fixed bottom-0 right-4 z-50 w-full max-w-[360px] sm:right-6 md:right-8">
-    <Betslip selectedBet={selectedBet} />
+        <Betslip selectedBet={selectedBet} />
       </div>
     </>
   );
